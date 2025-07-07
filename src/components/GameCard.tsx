c6dc1208e2138c3d0f57497d286a8cee7a9f6e77
andreiTeodorescu97 <andreiTeodorescu97>
2025-07-07 15:20:12 +0300

    refacotr

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,27 +3,24 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
-import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <GameCardContainer>
-      <Card>
-        <Image src={getCroppedImageUrl(game.background_image)}></Image>
-        <CardBody>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <HStack justifyContent="space-between">
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            ></PlatformIconList>
-            <CriticScore score={game.metacritic}></CriticScore>
-          </HStack>
-        </CardBody>
-      </Card>
-    </GameCardContainer>
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
+      <CardBody>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          ></PlatformIconList>
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
+      </CardBody>
+    </Card>
   );
 };
 
