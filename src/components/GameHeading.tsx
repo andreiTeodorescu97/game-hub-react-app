b476531fb1e3ab47566ed6debbbd9c53d85e602c
andreiTeodorescu97 <andreiTeodorescu97>
2025-07-08 08:53:32 +0300

    test

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -7,7 +7,7 @@ interface Props {
 const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${gameQuery.platform?.name || ""} ${
     gameQuery.genre?.name || ""
-  } Games`;
+  } Games Test Vercel`;
   return (
     <Heading marginY={5} fontSize="5xl" as="h1">
       {heading}
