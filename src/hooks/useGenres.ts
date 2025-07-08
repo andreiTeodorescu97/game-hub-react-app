9f289db964560af7cce11992ed9287259aa54469
andreiTeodorescu97 <andreiTeodorescu97>
2025-07-08 08:35:07 +0300

    Added static data for genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -7,7 +8,8 @@ export interface Genre {
 }
 
 const useGenres = () => {
-  return useData<Genre>("genres");
+  return { data: genres, isLoading: false, error: null };
+  //return useData<Genre>("genres");
 };
 
 export default useGenres;
