a38f4544ae7ad1b0cbdbdff5fab2ec1559fa476e
andreiTeodorescu97 <andreiTeodorescu97>
2025-07-09 08:57:04 +0300

    Retired useData hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/apiClient";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/apiClient";
 import { GameQuery } from "../App";
 
 export interface Game {
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/apiClient";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/apiClient";
 
 export interface Platform {
   id: number;
