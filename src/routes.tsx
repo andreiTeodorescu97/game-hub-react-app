b11d815316aaf5c37e70e203251fa8e04f812726
andreiTeodorescu97 <andreiTeodorescu97>
2025-07-11 08:43:45 +0300

    setup routing first time

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import GameDetailPage from "./pages/GameDetailPage";
+import HomePage from "./pages/HomePage";
+import Layout from "./pages/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      { index: true, element: <HomePage></HomePage> },
+      { path: "games/:id", element: <GameDetailPage></GameDetailPage> },
+    ],
+  },
+]);
+
+export default router;
